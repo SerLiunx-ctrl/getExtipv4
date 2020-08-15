b817e1251fe678ec9c9d4530aeda9825e8380c5b
SerLiunx-ctrl <68679473+SerLiunx-ctrl@users.noreply.github.com>
2020-08-15 14:25:34 +0800

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,6 +1,7 @@
-//  using System.Text.RegularExpressions;
-//  using System.Threading.Tasks;
+//      using System.Text.RegularExpressions;
+//      using System.Threading.Tasks;
 
+//      请先引用
 
         public static string GetExternalIp()  //获取WLAN IP
         {
