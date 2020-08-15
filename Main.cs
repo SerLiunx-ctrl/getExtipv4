d052df63d9afa7937528ea74fa8367dbab6c3052
SerLiunx-ctrl <68679473+SerLiunx-ctrl@users.noreply.github.com>
2020-08-15 14:22:58 +0800

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,29 @@
+//  using System.Text.RegularExpressions;
+//  using System.Threading.Tasks;
+
+
+        public static string GetExternalIp()  //获取WLAN IP
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                client.Encoding = System.Text.Encoding.Default;
+                string response = client.DownloadString("http://ip.chinaz.com/");
+                string myReg = @"<dd class=""fz24"">([\s\S]+?)<\/dd>";
+                Match mc = Regex.Match(response, myReg, RegexOptions.Singleline);
+                if (mc.Success && mc.Groups.Count > 1)
+                {
+                    response = mc.Groups[1].Value;
+                    return response;
+                }
+                else
+                {
+                    return "error";
+                }
+            }
+            catch (Exception)
+            {
+                return "error";
+            }
+
+        }
